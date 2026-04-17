2395d23660b68f7051ee9269e79f27d1055e6d86
John Kelly <johnkelly.main@gmail.com>
2026-04-17 01:03:57 +0000

    feat(jkai): add Artifact dispatcher component

---
+++ src/lib/components/jkai/artifacts/Artifact.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { Artifact } from '$lib/workflows/site-tools/artifact-types';
+  import ChartArtifact from './ChartArtifact.svelte';
+  import MapArtifact from './MapArtifact.svelte';
+  import TableArtifact from './TableArtifact.svelte';
+
+  let { artifact }: { artifact: Artifact } = $props();
+</script>
+
+{#if artifact.type === 'chart'}
+  <ChartArtifact {artifact} />
+{:else if artifact.type === 'map'}
+  <MapArtifact {artifact} />
+{:else if artifact.type === 'table'}
+  <TableArtifact {artifact} />
+{/if}
