2a6470b3668264e9dbd1806f70be62828b164be0
John Kelly <johnkelly.main@gmail.com>
2026-04-29 11:37:46 +0000

    feat(umami): inject tracker script on /blog/*

---
+++ src/lib/components/UmamiTracker.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { env } from '$env/dynamic/public';
+  const siteId = env.PUBLIC_UMAMI_SITE_ID;
+  const scriptUrl = env.PUBLIC_UMAMI_SCRIPT_URL;
+</script>
+
+{#if siteId && scriptUrl}
+  <svelte:head>
+    <script async defer data-website-id={siteId} src={scriptUrl}></script>
+  </svelte:head>
+{/if}
---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import UmamiTracker from '$lib/components/UmamiTracker.svelte';
+  let { children } = $props();
+</script>
+
+<UmamiTracker />
+{@render children()}
